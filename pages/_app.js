c56da20f4200f0f932f89cfbc9cefb77753118b3
Biao Shao <shaobiao@BiaodeMacBook-Pro.local>
2019-03-27 15:57:35 +0800

    eslint

--- pages/_app.js
+++ pages/_app.js
@@ -1,40 +1,35 @@
-import React from 'react'
-import App, { Container } from "next/app";
+import React from "react";
+import { Container } from "next/app";
 import Head from "next/head";
 import { renderLayout } from "../utils/render-layouts";
-import Post from '../components/Post'
-import './_app.less'
+import "./_app.less";
 
 class IndexPage extends React.Component {
-  constructor(props) {
-      super(props);
-      this.state = { };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
 
-  static async getInitialProps({ Component, router, ctx }) {
-      let pageProps = {};
+    static async getInitialProps({ Component, ctx }) {
+        let pageProps = {};
 
-      if (Component.getInitialProps) {
-          pageProps = await Component.getInitialProps(ctx);
-      }
-      return { ...pageProps };
-  }
+        if (Component.getInitialProps) {
+            pageProps = await Component.getInitialProps(ctx);
+        }
+        return { ...pageProps };
+    }
 
-  render() {
-    const {
-      Component
-    } = this.props
-    sss = ss + "...";
-    return (
-      <Container>
-        <Head>
-            <meta name="keywords" content="...." />
-        </Head>
-        <React.Fragment>
-            {renderLayout(this.props, this.state)}
-        </React.Fragment>
-      </Container>
-    )
-  }
+    render() {
+        return (
+          <Container>
+            <Head>
+              <meta name="keywords" content="...." />
+            </Head>
+            <React.Fragment>
+              {renderLayout(this.props, this.state)}
+            </React.Fragment>
+          </Container>
+        );
+    }
 }
-export default IndexPage
+export default IndexPage;
