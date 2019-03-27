ab760b1680a7c236564e21ceb30473725d6f769e
Biao Shao <shaobiao@BiaodeMacBook-Pro.local>
2019-03-27 18:29:00 +0800

    pm2

--- next.config.js
+++ next.config.js
@@ -1,33 +1,37 @@
-const path = require('path')
-const withPlugins = require('next-compose-plugins');
-const withCss = require('@zeit/next-css');
-const withLess = require('@zeit/next-less');
+// const path = require("path");
+const withPlugins = require("next-compose-plugins");
+const withCss = require("@zeit/next-css");
+const withLess = require("@zeit/next-less");
 
-module.exports = withPlugins([withCss, withLess({
-  cssModules: true
-})], {
-  module:{
-      rules:[
-          {
-            test: /\.css$/,
-            use: [
-              'style-loader',
-              'css-loader'
+module.exports = withPlugins(
+    [
+        withCss,
+        withLess({
+            cssModules: true
+        })
+    ],
+    {
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: ["style-loader", "css-loader"]
+                }
             ]
-          }
-      ]
-  },
-  webpack: (config, { dev }) => {
-    // Perform customizations to webpack config
+        },
+        assetPrefix: process.env.NODE_ENV === "production" ? "" : "",
+        webpack: config => {
+            // Perform customizations to webpack config
 
-    // Important: return the modified config
-    // config.resolve.modules = [path.resolve(__dirname, "components"), "node_modules"]
-    return config
-  },
-  webpackDevMiddleware: (config) => {
-    // Perform customizations to webpack dev middleware config
+            // Important: return the modified config
+            // config.resolve.modules = [path.resolve(__dirname, "components"), "node_modules"]
+            return config;
+        },
+        webpackDevMiddleware: config => {
+            // Perform customizations to webpack dev middleware config
 
-    // Important: return the modified config
-    return config
-  }
-});
+            // Important: return the modified config
+            return config;
+        }
+    }
+);
